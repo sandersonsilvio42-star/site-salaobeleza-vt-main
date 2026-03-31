0056f5b1328f6e94a582dfb7ee55595691273e58
sandersonsilvio42-estrela <sanderson_silvio42@hotmail.com>
2026-03-30 21:24:30 -0300

    arquivos excluidos no momento do commit

--- sw.js
+++
@@ -1,4 +0,0 @@
-// Este código faz o seu site ser reconhecido como um Aplicativo (PWA)
-self.addEventListener('fetch', function(event) {
-  // O site funciona normalmente, mas o Chrome agora aceita a instalação completa
-});
