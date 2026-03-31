6e1b1671747a4e6facd72a9bcba84149acdb21f6
sandersonsilvio42-estrela <sanderson_silvio42@hotmail.com>
2026-03-30 21:06:21 -0300

    instalando app no chrome

---
+++ sw.js
@@ -0,0 +1,4 @@
+// Este código faz o seu site ser reconhecido como um Aplicativo (PWA)
+self.addEventListener('fetch', function(event) {
+  // O site funciona normalmente, mas o Chrome agora aceita a instalação completa
+});
